d2f7aac9371ec1c6a1aff070650e2d08b455ca5f
xhx <xiaohuixu@126.com>
2025-02-14 18:41:52 +0800

    monorepo构建

--- packages/components/Button/index.vue
+++ packages/components/Button/index.vue
@@ -5,16 +5,16 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent } from 'vue';
 export default defineComponent({
-  name: "MyButton",
+  name: 'MyButton',
   props: {
     text: {
       type: String,
       required: true,
     },
   },
-  emits: ["click"],
+  emits: ['click'],
 });
 </script>
 
