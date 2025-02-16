178279d8d10cfd178ad2e390e28d7551c60e111e
jcz <xiaohuixu@126.com>
2025-02-16 23:54:54 +0800

    change: 测试地址配置

--- example/src/main.ts
+++ example/src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
-import XUI from '@x-ui/components/index.ts';
+import XUI from '@x-ui/components';
 import '@x-ui/components/dist/index.css';
 
 createApp(App).use(ElementPlus).use(XUI).mount('#app');
